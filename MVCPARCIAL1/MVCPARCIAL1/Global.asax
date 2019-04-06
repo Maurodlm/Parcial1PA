5d9589160f7be2c369d49571008adf92ef9f1975
MAURO ALEJANDRO HERNANDEZ DE LAS MUÑECAS <mauroahdlm@gmail.com>
2019-04-06 12:18:45 -0400

    commit proyecto MVCPARCIAL1

---
+++ MVCPARCIAL1/MVCPARCIAL1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCPARCIAL1.MvcApplication" Language="C#" %>
---
+++ MVCPARCIAL1/MVCPARCIAL1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCPARCIAL1.Startup))]
+namespace MVCPARCIAL1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
